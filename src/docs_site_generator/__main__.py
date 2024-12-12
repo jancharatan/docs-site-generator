0469ad6b8631ef1ff52e0ba35ec761c9a37b9a25
Jan Charatan <jan.charatan@gmail.com>
2024-12-11 22:35:31 -0500

    create github action (probably won't work)

--- src/docs_site_generator/__main__.py
+++ src/docs_site_generator/__main__.py
@@ -3,13 +3,18 @@ from docs_fetching.api_accessor import get_credentials, get_folder_id, get_files
 from site_generation.builder import make_build
 
 def main():
+    print("Starting site generation...")
     folder_name = "docs-site-generator"
+
+    print("Getting credentials...")
     credentials = get_credentials()
 
+    print("Fetching information from files...")
     drive_service = build("drive", "v3", credentials=credentials)
     folder_id = get_folder_id(folder_name, drive_service)
     files = get_files_in_folder(folder_id, drive_service)
 
+    print("Building site...")
     docs_service = build('docs', 'v1', credentials=credentials)      
     make_build({ file.name: get_content(file, docs_service) for file in files })
 
